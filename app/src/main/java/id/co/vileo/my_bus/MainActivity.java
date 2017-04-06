7539056919ab552cbf02e519d0b7e281fd299623
suhe <hendarsyahss@gmail.com>
2017-04-06 23:29:16 +0800

    Add

---
+++ app/src/main/java/id/co/vileo/my_bus/MainActivity.java
@@ -0,0 +1,13 @@
+package id.co.vileo.my_bus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/id/co/vileo/my_bus/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package id.co.vileo.my_bus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(i);
+                finish(); //close this activity
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
